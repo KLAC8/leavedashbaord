4e143f1f7a7ebf3a2058abf853e61a25f8e805d9
KLAC8 <ahusan.kelaalac@gmail.com>
2025-07-17 15:47:43 +0500

    all seeming errors are kinda fixed

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -4,19 +4,26 @@ import { MongoClient } from 'mongodb';
 const uri = process.env.MONGODB_URI!;
 const options = {};
 
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
+declare global {
+  // Add a type declaration for the global property we use to cache the client promise
+  // This ensures no usage of `any`
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Please add your Mongo URI to .env.local');
 }
 
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
 if (process.env.NODE_ENV === 'development') {
-  if (!(global as any)._mongoClientPromise) {
+  if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
-    (global as any)._mongoClientPromise = client.connect();
+    global._mongoClientPromise = client.connect();
   }
-  clientPromise = (global as any)._mongoClientPromise;
+  clientPromise = global._mongoClientPromise;
 } else {
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
