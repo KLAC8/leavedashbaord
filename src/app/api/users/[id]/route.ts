9083e356faebf3e0e9ad3736e331db3be4d4c9a4
KLAC8 <ahusan.kelaalac@gmail.com>
2025-07-20 10:05:44 +0500

    DYNAMIC USERES ROUTE UPDATED

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -2,15 +2,17 @@ import { connectDB } from '@/lib/db';
 import Employee from '@/models/Employee';
 import { NextResponse } from 'next/server';
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+export async function DELETE(req: Request, { params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
   await connectDB();
-  await Employee.findByIdAndDelete(params.id);
+  await Employee.findByIdAndDelete(id);
   return NextResponse.json({ success: true });
 }
 
-export async function PATCH(req: Request, { params }: { params: { id: string } }) {
+export async function PATCH(req: Request, { params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
   const { role } = await req.json();
   await connectDB();
-  await Employee.findByIdAndUpdate(params.id, { role });
+  await Employee.findByIdAndUpdate(id, { role });
   return NextResponse.json({ success: true });
 }
